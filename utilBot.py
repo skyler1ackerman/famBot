f1a2206fe38c777ba4157b474c01cedbf193b9ea
Skyler Ackerman <skyler1ackerman@gmail.com>
2021-03-19 18:30:17 -0700

    Added a few util functions

--- utilBot.py
+++ utilBot.py
@@ -1,5 +1,7 @@
-import discord
+import discord, json, asyncio
+from random import choice
 from discord.ext import commands
+from nltk import tokenize
 
 # Custom Errors
 class NotAnAdmin(commands.CheckFailure):
@@ -25,6 +27,26 @@ class UtilBot(commands.Cog):
 	async def repeat(self, ctx, rString, num):
 		for x in range(int(num)):
 			await ctx.send(rString)
+
+	# @commands.command(name='google', aliases=['g'])
+	# async def google(self, ctx, gString):
+
+	@commands.command(name='joke', aliases=['j'])
+	async def joke(self, ctx):
+		with open('jokes.json') as json_file: 
+   			randEl = choice(json.load(json_file))
+   			await ctx.send(randEl['setup'])
+   			await asyncio.sleep(3)
+   			await ctx.send(randEl['punchline'])
+
+	@commands.command(name='MB')
+	async def moAndBo(self,ctx):
+		with open('M_B.txt', 'r') as f:
+   			data=tokenize.sent_tokenize(f.read())
+		for line in data:
+			await ctx.send(line)
+			await asyncio.sleep(2)
+
 
 	# Regular shut down of the bot
 	@commands.command(name='s')
