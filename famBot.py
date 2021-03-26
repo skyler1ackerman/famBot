e3e0fea61d52090e2421fb350d9b5cf3918389f3
unknown <stackerm@ucsd.edu>
2021-03-26 08:37:04 -0700

    Added a food bot, quotes bot, loveme func, and a couple of utility functions

--- famBot.py
+++ famBot.py
@@ -6,7 +6,8 @@ from discord.ext import commands
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-extensions = ['raidBot', 'utilBot', 'leagueBot']
+extensions = ['raidBot', 'utilBot', 'foodBot', 'imgBot', 'leagueBot']
+
 for exten in extensions:
 	bot.load_extension(exten)
 
